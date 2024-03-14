bb42078c3ea8bbae3ca2675d8b5b087e6664f682
stealacamera <113174127+stealacamera@users.noreply.github.com>
2024-03-14 21:42:43 +0100

    Relocated backend files

---
+++ backend/WaterFilterBusiness/WaterFilterBusiness.DAL/Entities/InventoryItem.cs
@@ -0,0 +1,7 @@
+﻿namespace WaterFilterBusiness.DAL.Entities;
+
+public class InventoryItem : Entity
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ backend/WaterFilterBusiness/WaterFilterBusiness.DAL/EntityConfigurations/InventoryItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WaterFilterBusiness.DAL.Entities;
+
+namespace WaterFilterBusiness.DAL.EntityConfigurations;
+
+internal class InventoryItemConfiguration : IEntityTypeConfiguration<InventoryItem>
+{
+    public void Configure(EntityTypeBuilder<InventoryItem> builder)
+    {
+        
+    }
+}
