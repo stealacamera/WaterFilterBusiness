edb26fb7bdf60e2232f5462915a55fdba74acb06
stealacamera <113174127+stealacamera@users.noreply.github.com>
2024-03-18 20:39:23 +0100

    Added unit of work

---
+++ backend/WaterFilterBusiness/WaterFilterBusiness.DAL/WorkUnit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace WaterFilterBusiness.DAL;
+
+public interface IWorkUnit
+{
+    Task SaveChangesAsync();
+    Task<IDbContextTransaction> BeginTransactionAsync();
+}
+
+internal sealed class WorkUnit
+{
+    private readonly AppDbContext _dbContext;
+
+    public WorkUnit(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<IDbContextTransaction> BeginTransactionAsync()
+    {
+        return await _dbContext.Database.BeginTransactionAsync();
+    }
+}
